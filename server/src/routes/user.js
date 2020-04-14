3ca7c0a66b8d90824fa20a18468186a59ccb0106
William Cavalcante <williambreno@mat.ci.ufpb.br>
2020-04-14 18:12:34 -0300

    user login endpoint to POST

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -2,9 +2,9 @@ import express from 'express';
 
 let userRouter = express.Router();
 
-userRouter.get("/", async (req, res) => {
-    const email = req.query.email;
-    const password = req.query.password;
+userRouter.post("/login", async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
 
     const rs = await req.models.userModel.getUserByEmail(email);
     res.send(rs);
@@ -13,6 +13,10 @@ userRouter.get("/", async (req, res) => {
 userRouter.post("/", async (req, res) => {
     const rs = await req.models.userModel.createUser(req.body);
     res.send(rs);
+});
+
+userRouter.get("/me", async (req, res) => {
+    res.send('you');
 });
 
 export default userRouter;
