0a5b1bcda5225fd7182e8857729dac0770b1c498
William Cavalcante <williambreno@mat.ci.ufpb.br>
2020-04-14 19:52:57 -0300

    quoted variables

--- server/src/models/db.js
+++ server/src/models/db.js
@@ -3,8 +3,8 @@ import { Pool } from 'pg';
 const pool = new Pool ({
     user: "postgres",
     host: process.env.DB_HOST,
-    database: "food_order",
-    password: "admin",
+    database: process.env.DB,
+    password: process.env.DB_PASSWORD,
     port: 5432
 });
 
