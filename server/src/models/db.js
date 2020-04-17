60c4026ba6b0f4f605c0e2ba4fc475807f216f70
William Cavalcante <williambreno@mat.ci.ufpb.br>
2020-04-16 22:32:50 -0300

    removed console logs

--- server/src/models/db.js
+++ server/src/models/db.js
@@ -3,9 +3,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-console.log(process.env.DB);
-console.log(process.env.DB_PASSWORD);
-
 const pool = new Pool({
   user: "postgres",
   host: process.env.DB_HOST,
